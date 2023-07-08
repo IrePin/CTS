1a587d28d49bd51a36177ca8590fa2f48b5393dc
IrePin <gagistos@gmail.com>
2023-07-08 13:22:36 +0300

    TankCube Fix

--- Assets/TankSimSystem/Scripts/Tank/TankCube.cs
+++ Assets/TankSimSystem/Scripts/Tank/TankCube.cs
@@ -5,18 +5,15 @@ namespace TankSimSystem
 {
     public class TankCube : MonoBehaviour
     {
-        private const float MaxWaterLevel = 10f;
         [field: SerializeField] public float WaterLevel { get; private set; }
-
-        [Header("TankVolume")] 
-        
+        private const float MaxWaterLevel = 10f;
         [SerializeField] private float height;
-        [SerializeField] private  float length;
+        [SerializeField] private float length;
         [SerializeField] private float width;
 
-
-        public float TankVolume => length * width * height; 
-        public float WaterVolume => TankVolume * (WaterLevel / 10);
+       
+        public float TankVolume => length * width * height;
+        public float WaterVolume => TankVolume * (WaterLevel / MaxWaterLevel);
         public float WaterLevelNormalized => WaterLevel / MaxWaterLevel;
         public event Action<float> OnWaterLevelChangedNormalized;
 
@@ -30,14 +27,12 @@ namespace TankSimSystem
         {
             WaterLevel = waterLevel;
             OnWaterLevelChangedNormalized?.Invoke(WaterLevelNormalized);
-            Debug.Log("WaterLevelNormalized = " + WaterLevelNormalized);
-            Debug.Log("TankVolume = " + TankVolume);
-            Debug.Log("WaterVolume = " + WaterVolume);
         }
+
         public void AddVolume(float volume)
         {
-            var dh = volume / TankVolume;
-            SetWaterLevel(WaterLevel + dh);
+            var waterLevelChange  = volume / TankVolume;
+            SetWaterLevel(WaterLevel + waterLevelChange);
         }
     }
 }
